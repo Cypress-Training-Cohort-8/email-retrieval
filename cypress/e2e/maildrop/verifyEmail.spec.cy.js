d28403053c1baffac479c45ee25ec81abc540ce0
Ademola Bhadmus <bhadmusautomates1@gmail.com>
2025-11-16 16:06:05 +0000

    added link retrieval

--- cypress/e2e/maildrop/verifyEmail.spec.cy.js
+++ cypress/e2e/maildrop/verifyEmail.spec.cy.js
@@ -1,8 +1,7 @@
 import { fakerEN_NG as faker } from "@faker-js/faker";
 let data
-const emailDomain = '@wtzlsjfo.mailosaur.net'
-const serverID = 'wtzlsjfo'
-let emailAddress
+const username = `test${faker.string.uuid()}`
+const emailAddress = `${username}@maildrop.cc`
 context('Test to verify email link', ()=>{
     before(()=>{
         cy.fixture('selectors').then(ele=>{
@@ -14,7 +13,6 @@ context('Test to verify email link', ()=>{
         cy.clickElement(data.qaBrains.signInBtn)
         cy.clickElement(data.qaBrains.signUpLnk)
         cy.insertText(data.qaBrains.nameFld, faker.person.fullName())
-        emailAddress = `test${faker.string.uuid()}${emailDomain}`
         cy.insertText(data.qaBrains.emailFld, emailAddress)
         cy.get('select[name="country_id"]').select('Nigeria')
         cy.get("select[name='profession']").select('Personal')
@@ -22,11 +20,6 @@ context('Test to verify email link', ()=>{
         cy.insertText(data.qaBrains.confPassFld, 'Test@1234')
         cy.get('input#terms').check()
         cy.clickElement(data.qaBrains.signUpBtn)
-        cy.mailosaurGetMessage(serverID, { sentTo: emailAddress})
-        .then((email)=>{
-            expect(email.subject).to.include('Welcome to QA Brains! Think Quality, Think QA Brains.')
-            const verifyLink = email.html.links[0].href
-            cy.visit(verifyLink)
-        })
+        cy.verifyEmailLink(username)
     })
 })
